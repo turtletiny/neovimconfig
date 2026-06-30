020f6c30c34f23bd0979f249b0d392bf067cc9fe
Daniel <daniel.lukins1@gmail.com>
2026-06-30 14:04:01 +1000

    intalled custom plugins: neotree, indent line

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,4 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
+-- Custom Plugins
 -- See the kickstart.nvim README for more information
 
 -- Iterate over all Lua files in the plugins directory and load them
---
+++ lua/custom/plugins/java.lua
@@ -0,0 +1,4 @@
+-- Neovim Java JDTLS support configuration
+-- Sourced automatically by lua/custom/plugins/init.lua
+
+vim.pack.add { 'https://github.com/mfussenegger/nvim-jdtls' }
