aa1179d3877a65cd063effce99007139caaaefd9
Daniel <daniel.lukins1@gmail.com>
2026-06-30 14:08:05 +1000

    added linting

--- init.lua
+++ init.lua
@@ -920,10 +920,10 @@ end
 do
   -- require 'kickstart.plugins.debug'
   require 'kickstart.plugins.indent_line'
-  -- require 'kickstart.plugins.lint'
+  require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
-  -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   require 'custom.plugins'
 end
